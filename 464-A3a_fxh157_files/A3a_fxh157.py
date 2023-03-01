f50d6f53cbee4d07ba7bc9bc10431a9dddecc256
finnhittson <hittson.finn@gmail.com>
2023-03-01 12:01:36 -0500

    add A3b main file and start assignment

--- 464-A3a_fxh157_files/A3a_fxh157.py
+++ 464-A3a_fxh157_files/A3a_fxh157.py
@@ -8,25 +8,29 @@ import A1b_fxh157 as a1b
 
 # 1. Continuous signals and sampling
 ## 1a. Sampled functions
-def plot_sampled_function(g, fs:int=1, tlim:tuple=None, tscale:float=1.0, tunits:str="sec", **kwargs):
-    t = np.arange(tlim[0], tlim[1], tlim[1]/(100000))
+def plot_sampled_function(g, fs:int=1, tlim:tuple=None, tscale:float=1.0, tunits:str="sec", title:str="set me", **kwargs):
+    t = np.arange(tlim[0], tlim[1], tlim[1]/(100000))*tscale
     y = g(t=t, **kwargs)
     plt.plot(t, y)
+
     norm = 1
     if g is a1b.gammatone:
         norm = a1b.gammatone_norm(y)
-
-    sample_times = np.arange(tlim[0], tlim[1], tscale/fs)
+    
+    sample_times = np.arange(tlim[0], tlim[1], 1/fs)
     if g is a1b.sinewave:
-        sample_times = np.arange(tlim[0], tlim[1], 1/fs)*tscale
+        sample_times = np.arange(tlim[0]*tscale, tlim[1]*tscale, 1/fs)
+    #'''
     for st in sample_times:
         y = g(t=st, **kwargs)/norm
         plt.plot([st, st], [0, y], 'r')
         plt.scatter(st, y, c='r', s=15, zorder=10)
-    
+    #'''
     plt.xlim([t[0], t[-1]])
     plt.xlabel(f"time $t$, ({tunits})", fontsize=16)
     plt.ylabel("amplitude", fontsize=16)
+    plt.title(title, fontsize=18)
+    plt.show()
 
 # 2. Signals
 ## 2a. Delta and step functions
@@ -53,20 +57,20 @@ def plot_delta_step(t, fs, g, plot_type:str="line"):
     plt.show()
 
 ## 2b. gensignal
-def gensignal(t, g, fs:int=1, tau:float=1.0, T:float=1.0, **kwargs):
+def gensignal(t, g, fs:int=1, tau:float=1.0, T:float=1.0, tscale:float=0.001, **kwargs):
     if g == d:
-        t = np.arange(t, t+T-tau+1, 1/fs)
+        t = np.arange(t, t+T+tau, 1/(tscale*fs))
         y = g(t, **kwargs)
         return t + np.ones(len(t))*tau, y
-    t = np.arange(t+tau, T+tau, (T-t)/fs)
+    t = np.arange(t+tau, T+tau, 1/(tscale*fs))
     y = g(t, **kwargs)
     return t, y
 
-def plot_stem(t, y, title):
+def plot_stem(t, y, title, time_units):
     for i in range(len(y)):
         plt.plot([t[i], t[i]], [0, y[i]], '#1f77b4')
         plt.scatter(t[i], y[i], c='#1f77b4', s=15)
-    plt.xlabel("time $t$, (sec)", fontsize=16)
+    plt.xlabel(f"time $t$, ({time_units})", fontsize=16)
     plt.ylabel("amplitude", fontsize=16)
     plt.title(title, fontsize=18)
     plt.show()
@@ -83,14 +87,14 @@ def snr(Ps, Pn):
     return Ps/Pn
 
 # 3b. Noisy signals
-def noisysignal(t, g, fs, tau, T, s, **kwargs):
-    t, signal = gensignal(t=t, g=g, fs=fs, tau=tau, T=T, **kwargs)
+def noisysignal(t, g, fs, tau, T, s, tscale:float=0.001, **kwargs):
+    t, signal = gensignal(t=t, g=g, fs=fs, tau=tau, T=T, tscale=tscale, **kwargs)
     noise = np.random.normal(loc=0, scale=s, size=len(signal))
     return t, signal, noise
 
-def plot_noisysignal(t, y, title):
+def plot_noisysignal(t, y, title, tunits):
     plt.plot(t, y, linewidth=0.5)
-    plt.xlabel("time $t$, (sec)", fontsize=16)
+    plt.xlabel(f"time $t$, ({tunits})", fontsize=16)
     plt.ylabel("amplitude", fontsize=16)
     plt.title(title, fontsize=16)
     plt.show()
