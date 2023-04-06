b79cb49bc03714992e17741be68f1a707f3ebaa4
finnhittson <hittson.finn@gmail.com>
2023-04-06 17:48:07 -0400

    A4: add harmonic and cosine functions

--- 464-A4-fxh157-files/A4_fxh157.py
+++ 464-A4-fxh157-files/A4_fxh157.py
@@ -20,13 +20,13 @@ def cosine(t, f:list=[1], alist:list=[1], phase_list:list=[0]):
 
 def plot_harmonics(t, g, f, alist:list=[1], phase_list:list=[0], title:str="set me"):
     y = [g(t=i, f=f, alist=alist, phase_list=phase_list) for i in t]
-    plt.figure().set_figheight(2)
-    plt.subplot(121)
+    fig, axs = plt.subplots(1,2, figsize=(7,2))
     if isinstance(f, int) or isinstance(f, float):
-        plt.stem([f*(i+1) for i in range(len(alist))], basefmt=" ")
+        axs[0].stem([f*(i+1) for i in range(len(alist))], basefmt=" ")
     else:
-        plt.stem(f, basefmt=" ")
-    plt.subplot(122)
-    plt.plot(t, y)
-    plt.title(title)
+        axs[0].stem(f, basefmt=" ")
+    axs[0].set_title("frequencies")
+    axs[0].set_xticks(ticks=list(range(0, len(alist))), labels=list(range(1, len(alist)+1)))
+    axs[1].plot(t, y)
+    axs[1].set_title(title)
     plt.show()
